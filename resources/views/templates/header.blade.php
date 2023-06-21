e40226cf868e667e0dca68cba9b5979fa2bdf539
Basilebanissi <basilebanissi02@gmail.com>
2023-06-21 18:03:27 +0000

    Initial commit

---
+++ resources/views/templates/header.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>HomePage</title>
+</head>
+<body>
+   <nav class="navbar">
+    <div>
+        <img src="logo.jpg" alt="Logo" class="logo">
+    </div>
+    <div>
+        <div>Acceuil</div>
+        <div>Opportunités</div>
+        <div>Ressources</div>
+        <div>Formations</div>
+        <div>Communauté</div>
+        <div>Incubateurs</div>
+    </div>
+    <div>
+        <div>Search barre</div>
+    </div>
+    <div>
+        <div>Se connecter</div>
+    </div>
+   </nav>
+   <div class="flashInfo">
+        <div class="flashInfoTitle">Info</div>
+        <div class="flashInfoBody">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa voluptates excepturi accusamus ipsa vero laudantium id porro tenetur, veniam maxime quis praesentium recusandae repudiandae, velit hic quaerat adipisci voluptatem? Accusantium.</div>
+   </div>
+</body>
+</html>
